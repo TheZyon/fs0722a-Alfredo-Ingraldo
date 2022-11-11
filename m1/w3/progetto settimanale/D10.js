e91365da05a210802335847138aae66e528ea38d
TheZyon <alfredo.ingraldo.u@gmail.com>
2022-11-11 10:12:09 +0100

    aggiunto progetto settimanale

--- m1/w3/progetto settimanale/D10.js
+++ m1/w3/progetto settimanale/D10.js
@@ -64,7 +64,7 @@ me.skills.pop();
 */
 
 function dice() {
-  return Math.round(Math.random() * 5+1);
+  return Math.round(Math.random() * 5 + 1);
 }
 
 /* ESERCIZIO 2
@@ -112,9 +112,9 @@ function onlyLetters(str) {
   });
   let str1 = "";
   for (let e of array1) {
-    str1 += e +" ";
+    str1 += e + " ";
   }
-  return str1.slice(0, str1.length-1); //togliamo l'ultimo spazio
+  return str1.slice(0, str1.length - 1); //togliamo l'ultimo spazio
 }
 
 function isNumeric(n) {
@@ -125,23 +125,22 @@ function isNumeric(n) {
   Crea una funzione chiamata "isThisAnEmail" che riceve una stringa come parametro e ritorna true se la stringa è un valido indirizzo email.
 */
 
-function isThisAnEmail(str) { //sfrutto il metodo checkValidity
+function isThisAnEmail(str) {
+  //sfrutto il metodo checkValidity
   let input = document.createElement("input");
   input.setAttribute("type", "email");
   input.value = str;
   return input.checkValidity();
 }
 
-
 /* ESERCIZIO 7
   Scrivi una funzione chiamata "whatDayIsIt" che ritorna il giorno della settimana corrente.
 */
-function whatDayIsIt() { 
+function whatDayIsIt() {
   //trasforma l'obj date in una stringa con toDateString()
   //poi spezza la stringa in un array e prende il primo elemento (che corrisponde al giorno)
   let date = new Date();
-  return date.toDateString().split(" ")[0]; 
-  
+  return date.toDateString().split(" ")[0];
 }
 
 /* ESERCIZIO 8
@@ -157,13 +156,13 @@ function whatDayIsIt() {
   }
 */
 
-function rollTheDices(n) { 
+function rollTheDices(n) {
   let objReturned = {
     sum: 0,
-    values: []
+    values: [],
   };
 
-  for (let i = 0; i < n; i++) { 
+  for (let i = 0; i < n; i++) {
     let res = dice();
     objReturned.sum += res;
     objReturned.values[objReturned.values.length] = res;
@@ -171,19 +170,93 @@ function rollTheDices(n) {
   return objReturned;
 }
 
-
-
 /* ESERCIZIO 9
   Scrivi una funzione chiamata "howManyDays" che riceve una data come parametro e ritorna il numero di giorni trascorsi da tale data.
 */
 
+let dateToCmp = new Date();
 
+function howManyDays(date) {
+  let currentDate = new Date();
+  let year1 = date.getFullYear();
+  let year2 = currentDate.getFullYear();
+  let d = mostRecent(date, currentDate); //d è un array con d[0] data precedente nel tempo a d[1]
+  let r = giorniDaInizioAnno(d[0]) + giorniAFineAnno(d[1]); //una quantità di giorni che serve dopo
+  return (Math.abs(year1 - year2) + 1) * 365 - r - 1;
+  //il primo addendo da i giorni che stanno tra il primo anno e l'ultimo anno,
+  //poi ci sottraggo r per ottenere i giorni che sono passati veramente
+}
 
+function mostRecent(date1, date2) {
+  let arr = [date1, date2];
+  if (date1.getFullYear() != date2.getFullYear()) {
+    return arr.sort((a, b) => {
+      return compare(a.getFullYear(), b.getFullYear());
+    }); //riordino arr comparando gli anni e ho finito
+  } else if (date1.getMonth() != date2.getMonth()) {
+    //confrontiamo i mesi
+    return arr.sort((a, b) => {
+      return compare(a.getMonth(), b.getMonth());
+    }); //idem di sopra ma con i mesi
+  } else {
+    //anche i mesi sono uguali
+    return arr.sort((a, b) => {
+      return compare(a.getDate(), b.getDate());
+    }); //idem di sopra ma con i giorni
+  }
+}
 
+function giorniDaInizioAnno(date) {
+  //ritorna il numero di giorni dall'inizio dell'anno della data al giorno della data
+  let sum = 0;
+  let date1 = date;
+  let year = date.getFullYear();
+  while (date1.getFullYear() == year) {
+    date1.setDate(date1.getDate() - 1);
+    sum += 1;
+  }
+  sum -= 1;
+  return sum;
+}
+
+function giorniAFineAnno(date) {
+  //idem alla funzione di sopra, ma restituisce i giorni che mancano alla fine dell'anno
+  let sum = 0;
+  let date1 = date;
+  let year = date.getFullYear();
+  while (date1.getFullYear() == year) {
+    date1.setDate(date1.getDate() + 1);
+    sum += 1;
+  }
+  sum -= 1;
+  return sum;
+}
+
+function compare(a, b) {
+  switch (true) {
+    case a < b:
+      return -1;
+    case a > b:
+      return 1;
+    default:
+      return 0;
+  }
+}
 
 /* ESERCIZIO 10
   Scrivi una funzione chiamata "isTodayMyBirthday" che deve ritornare true se oggi è il tuo compleanno, falso negli altri casi.
 */
+
+function isTodayMyBirthday() {
+  let birthday = new Date();
+  birthday.setDate(9);
+  birthday.setMonth(3);
+  if (howManyDays(birthday) == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 // Arrays & Oggetti
 
@@ -194,70 +267,178 @@ function rollTheDices(n) {
   in esso la proprietà chiamata come la stringa passata come secondo parametro.
 */
 
+class Persona {
+  constructor(
+    name = "mario",
+    surname = "rossi",
+    age = 50,
+    qi = 80,
+    votaGiorgia = true
+  ) {
+    this.name = name;
+    this.surname = surname;
+    this.age = age;
+    this.qi = qi;
+    this.votaGiorgia = votaGiorgia;
+  }
+}
+
+let mariello = new Persona();
+
+function deleteProp(obj, str) {
+  delete obj[str];
+  return obj;
+}
+
 /* ESERCIZIO 12
   Scrivi una funzione chiamata "newestMovie" che trova il film più recente nell'array "movies" fornito.
 */
 
+function newestMovie() {
+  let newestMovie = { Year: 0 };
+  for (let movie of movies) {
+    if (eval(movie.Year) >= eval(newestMovie.Year)) {
+      newestMovie = movie;
+    }
+  }
+  return newestMovie;
+}
+
 /* ESERCIZIO 13
   Scrivi una funzione chiamata countMovies che ritorna il numero di film contenuti nell'array "movies" fornito.
 */
-
+function countMovies() {
+  return movies.length;
+}
 /* ESERCIZIO 14
   Scrivi una funzione chiamata "onlyTheYears" che crea un array con solamente gli anni di uscita dei film contenuti nell'array "movies" fornito.
 */
+function onlyTheYears() {
+  let arr = [];
+  for (let movie of movies) {
+    arr[arr.length] = movie.Year;
+  }
+  return arr;
+}
 
 /* ESERCIZIO 15
   Scrivi una funzione chiamata "onlyInLastMillennium" che ritorna solamente i film prodotto nel millennio scorso contenuti nell'array "movies" fornito.
 */
+function onlyInLastMillennium() {
+  let arr = [];
+  for (let movie of movies) {
+    if (eval(movie.Year) < 2000) {
+      arr[arr.length] = movie;
+    }
+  }
+  return arr;
+}
 
 /* ESERCIZIO 16
   Scrivi una funzione chiamata "sumAllTheYears" che ritorna la somma di tutti gli anni in cui sono stati prodotti i film contenuti nell'array "movies" fornito.
 */
 
+function sumAllTheYears() {
+  let sum = 0;
+  for (let movie of movies) {
+    sum += movie.Year;
+  }
+  return sum;
+}
+
 /* ESERCIZIO 17
   Scrivi una funzione chiamata "searchByTitle" che riceve una stringa come parametro e ritorna i film nell'array "movies" fornito che la contengono nel titolo.
 */
+
+function searchByTitle(str) {
+  return movies.filter((movie) => {
+    return movie.Title.includes(str);
+  });
+}
 
 /* ESERCIZIO 18
   Scrivi una funzione chiamata "searchAndDivide" che riceve una stringa come parametro e ritorna un oggetto contenente due array: "match" e "unmatch".
   "match" deve includere tutti i film dell'array "movies" fornito che contengono la stringa fornita all'interno del proprio titolo, mentre "unmatch" deve includere tutti i rimanenti.
 */
 
+function searchAndDivide(str) {
+  let obj = {};
+  obj.match = searchByTitle(str);
+  obj.unmatch = movies.filter((movie) => {
+    return !movie.Title.includes(str);
+  });
+  return obj;
+}
+
 /* ESERCIZIO 19
   Scrivi una funzione chiamata "removeIndex" che riceve un numero come parametro e ritorna l'array "movies" fornito privo dell'elemento nella posizione ricevuta come parametro.
 */
+function removeIndex(n) {
+  movies.splice(n, 1);
+  return movies;
+}
 
 // DOM (nota: gli elementi che selezionerai non si trovano realmente nella pagina)
 
 /* ESERCIZIO 20
   Scrivi una funzione per selezionare l'elemento dotato di id "container" all'interno della pagina.
 */
-
+function selectContainer() {
+  return document.getElementById("container");
+}
 /* ESERCIZIO 21
   Scrivi una funzione per selezionare ogni tag <td> all'interno della pagina.
 */
+function selectTd() {
+  return document.querySelectorAll("td");
+}
 
 /* ESERCIZIO 22
   Scrivi una funzione che, tramite un ciclo, stampa in console il testo contenuto in ogni tag <td> all'interno della pagina.
 */
-
+function printTds() {
+  let arr = selectTd();
+  for (let td of arr) {
+    console.log(td.innerText);
+  }
+}
 /* ESERCIZIO 23
   Scrivi una funzione per aggiungere un background di colore rosso a ogni link all'interno della pagina.
 */
-
+function red() {
+  let arr = document.querySelectorAll("a");
+  for (let a of arr) {
+    a.style = "background-color:red";
+  }
+}
 /* ESERCIZIO 24
   Scrivi una funzione per aggiungere un nuovo elemento alla lista non ordinata con id "myList".
 */
+function add() {
+  let list = document.getElementById("myList");
+  list.appendChild(document.createElement("li"));
+}
 
 /* ESERCIZIO 25
   Scrivi una funzione per svuotare la lista non ordinata con id "myList".
 */
+function empty() {
+  let list = document.getElementById("myList");
+  for (let child of list.children) {
+    list.removeChild(child);
+  }
+}
 
 /* ESERCIZIO 26
   Scrivi una funzione per aggiungere ad ogni tag <tr> la classe CSS "test"
 */
-
-// [EXTRA] JS Avanzato
+function test() {
+  let arr = document.querySelectorAll("tr");
+  for (let tr of arr) {
+    tr.setAttribute("class", "test");
+  }
+}
+// [EXTRA] JS Avanzato (avanzato solo per chi ha votato Giorgia)
 
 /* ESERCIZIO 27
   Crea una funzione chiamata "halfTree" che riceve un numero come parametro e costruisce un mezzo albero di "*" (asterischi) dell'altezza fornita.
@@ -271,6 +452,22 @@ function rollTheDices(n) {
 
 */
 
+function halfTree(n) {
+  //Step 1: creo e appendo un div in cui inserirò l'albero
+  let div = document.createElement("div");
+  document.body.appendChild(div);
+
+  //Step 2: con due cicli for costruisco l'albero e lo metto nel div
+  for (let i = 1; i <= n; i++) {
+    let s = "";
+    for (let j = 0; j < i; j++) {
+      s += "*";
+    }
+    div.innerHTML += s + "<br>";
+    s = "";
+  }
+}
+
 /* ESERCIZIO 28
   Crea una funzione chiamata "tree" che riceve un numero come parametro e costruisce un albero di "*" (asterischi) dell'altezza fornita.
 
@@ -281,11 +478,60 @@ function rollTheDices(n) {
    ***
   *****
 
+
+ 
+
+
 */
+function tree(n) {
+  let div = document.createElement("div");
+  document.body.appendChild(div);
+  div.style = "white-space:pre"; //questo serve per preservare gli spazi nel testo che inserirò nel div
+
+  for (let i = 0; i < n; i++) {
+    let s = "";
+    let t = "";
+    for (k = 0; k < 2 * (n - i - 1); k++) {
+      /*ci serve solo per fare 2*(n-i-1) iterazioni che servono a mettere gli spazi a sinistra
+      si scopre che questo è il numero giusto per allineare graficamente gli asterischi 
+      in modo da formare un albero, e non n-i-1 come uno potrebbe pensare inizialmente*/
+      t += " ";
+    }
+    for (let j = 0; j < 2 * i + 1; j++) {
+      //in questo modo si fanno (i-esimo numero dispari)-iterazioni
+      s += "*";
+    }
+    div.innerHTML += t + s + "<br>";
+  }
+}
+//Questo lo sapeva fare anche Giorgia
+//infatti lo ha inserito nella biografia, nel capitolo sull'importanza del natale per la famiglia cristiana 
 
 /* ESERCIZIO 29
   Crea una funzione chiamata "isItPrime" che riceve un numero come parametro e ritorna true se il numero fornito è un numero primo.
 */
+
+function isItPrime(p) {
+  
+  if (p == 1) { return true; }
+  else {
+    let dIndex = 0;
+    for (let d = 1; d <= p; d++) {
+      if (p % d == 0) { dIndex++; }
+    }
+    switch (dIndex) {
+      case 2: return true;
+      default: return false;
+    }
+  }
+} 
+
+
+
+
+
+
+
 
 /* Questo array viene usato per gli esercizi. Non modificarlo. */
 
