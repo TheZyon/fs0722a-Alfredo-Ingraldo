1ad7fbb2b3b8643224d11815b8aba142e553be9a
TheZyon <alfredo.ingraldo.u@gmail.com>
2022-11-29 18:08:44 +0100

    aggiornamento

--- m2/w2/d2/esercitazione/index.js
+++ m2/w2/d2/esercitazione/index.js
@@ -1,3 +1,16 @@
-let welcomeSButton = document.getElementById("welcomeSummer").children[0].children[1];
-let saldiButton = document.getElementById("saldi").children[0].children[1];
 
+let buttons = [document.getElementById("welcomeSummer").children[0].children[1],
+               document.getElementById("saldi").children[0].children[1]];
+let state = [0, 0];
+
+
+for (let i = 0; i < state.length; i++)
+buttons[i].addEventListener("mousedown", function () {
+    if (state[i] == 0) {
+        buttons[i].parentElement.parentElement.children[1].setAttribute("class", "d-none");
+        state[i] = Math.abs(1 - state[i]);
+    } else {
+        buttons[i].parentElement.parentElement.children[1].setAttribute("class", "d-inline");
+        state[i] = Math.abs(1 - state[i]);
+    }
+ });
