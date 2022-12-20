838309394cc684ea0d7eea1f8d0ee1b86c7c9f31
TheZyon <alfredo.ingraldo.u@gmail.com>
2022-12-20 17:12:42 +0100

    aggiornamneto

--- m3/w1/d2/pratica/JS/operazioni.js
+++ m3/w1/d2/pratica/JS/operazioni.js
@@ -1,7 +1,42 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const conti_js_1 = require("./conti.js");
-var name = "Carlino";
+class SonAccount {
+    constructor() {
+        this.balanceInit = 0; //balanceInit inizializzato a 0
+    }
+    deposit(soddi) {
+        this.balanceInit += soddi;
+        return "depositati euro: " + `${soddi}`;
+    }
+    withdraw(soddi) {
+        if (this.balanceInit >= soddi) {
+            this.balanceInit -= soddi;
+            return "ritirati euro: " + `${soddi}`;
+        }
+        else {
+            return "errore nel procedimento dalla banca...vale a dire che sei povero";
+        }
+    }
+    stampaSaldoAttuale() {
+        return "Possiedi: " + this.balanceInit;
+    }
+    stampaStoria() {
+        return "";
+    }
+}
+class MotherAccount extends SonAccount {
+    constructor(nome, pass, interessi) {
+        super();
+        this.interessi = interessi * 0.01;
+        this.nome = nome;
+        this.pass = pass;
+    }
+    calcolaInteressi() {
+        let soldiDaUscire = this.balanceInit * this.interessi + this.balanceInit;
+        this.balanceInit = 0;
+        return `Hai raccattato: ${soldiDaUscire}€. Pomplimenti!`;
+    }
+}
+var namee = "Carlino";
 var pass = "CarlinoBellino";
 var interessi = 5;
 var btnPrelievo = document.getElementsByTagName("button")[0];
@@ -12,7 +47,7 @@ var btnEstrattoConto = document.getElementsByTagName("button")[2];
 var btnInteressi = document.getElementsByTagName("button")[3];
 var out = document.getElementById("outputAtm");
 //inizializzo il conto
-let contoCarlino = new conti_js_1.MotherAccount(name, pass, interessi);
+let contoCarlino = new MotherAccount(namee, pass, interessi);
 //funziuonamento della CTA per il prelievo
 btnPrelievo.addEventListener("mousedown", function () {
     out.innerHTML = contoCarlino.withdraw(inputPrelievo.value);
--- m3/w1/d2/pratica/JS/operazioni.ts
+++ m3/w1/d2/pratica/JS/operazioni.ts
@@ -1,7 +1,59 @@
-import { SonAccount, MotherAccount } from "./conti.js";
 
 
-var name :string = "Carlino";
+
+
+
+
+class SonAccount {
+    protected balanceInit: number; //private di modo che vi si possa accedere dall'interno della classe
+    constructor() {
+        this.balanceInit = 0; //balanceInit inizializzato a 0
+    }
+
+
+    deposit(soddi: number): string{ //aumento balanceInit e ritorno messaggio di successo
+        this.balanceInit += soddi;
+        return "depositati euro: " + `${soddi}`;
+    }
+    withdraw(soddi: number): string {//ritorno messaggio di errore oppure decremento balanceInit e ritorno messaggio di successo 
+        if (this.balanceInit >= soddi) {
+            this.balanceInit -= soddi;
+            return "ritirati euro: " + `${soddi}`;
+        }
+        else { return "errore nel procedimento dalla banca...vale a dire che sei povero"; }
+    }
+
+    stampaSaldoAttuale():string {
+        return "Possiedi: " + this.balanceInit;
+    }
+
+    stampaStoria(): string { 
+        return "";
+    }
+}
+class MotherAccount extends SonAccount { 
+    
+    nome: string;
+    pass:string;
+    private interessi: number; //all'apertura del conto, si fissa un tasso di interesse. Ad es. 10 corrisponde al 10%
+    constructor(nome:string, pass:string, interessi:number){ 
+        super();
+        this.interessi = interessi * 0.01;
+        this.nome = nome;
+        this.pass = pass;
+    }
+
+    calcolaInteressi(): string { //calcola interessi sulla percentuale e restituisce il balanceInit aumentato degli interessi
+        let soldiDaUscire: number = this.balanceInit * this.interessi + this.balanceInit;
+        this.balanceInit = 0;
+        return `Hai raccattato: ${soldiDaUscire}€. Pomplimenti!`;
+    }
+
+}
+
+
+
+var namee :string = "Carlino";
 var pass :string = "CarlinoBellino"
 var interessi: number = 5;
 var btnPrelievo = document.getElementsByTagName("button")[0];
@@ -13,7 +65,7 @@ var btnInteressi=document.getElementsByTagName("button")[3];
 var out:any = document.getElementById("outputAtm");
 
 //inizializzo il conto
-let contoCarlino = new MotherAccount(name, pass, interessi);
+let contoCarlino = new MotherAccount(namee, pass, interessi);
 
 //funziuonamento della CTA per il prelievo
 btnPrelievo.addEventListener("mousedown", function () { 
